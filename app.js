87816bc117cc46c7d830ad12ff13023d75947cfb
cyberbot777 <cyberbot7777777@gmail.com>
2025-04-29 19:19:56 -0500

    Updated version to resemble example with JS features

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const likeBtn = document.getElementById('like-btn');
+const likeBtn = document.getElementById("like-btn");
 let isLiked = false;
 
-likeBtn.addEventListener('click', () => {
-    isLiked = !isLiked;
-    likeBtn.classList.toggle('far', !isLiked);
-    likeBtn.classList.toggle('fas', isLiked);
-    likeBtn.classList.toggle('liked', isLiked);
+likeBtn.addEventListener("click", () => {
+  isLiked = !isLiked;
+  likeBtn.classList.toggle("far", !isLiked);
+  likeBtn.classList.toggle("fas", isLiked);
+  likeBtn.classList.toggle("liked", isLiked);
 });
